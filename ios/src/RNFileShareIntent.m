689e368d252d399d0902858a9dbe10ecc86ffcd8
Nick Badagliacca <nick@terraeclipse.com>
2019-10-30 14:05:56 -0700

    [attempt] Port code from react-native-share-extension

--- ios/src/RNFileShareIntent.m
+++ ios/src/RNFileShareIntent.m
@@ -14,7 +14,92 @@
 static NSItemProvider* ShareFileIntentModule_itemProvider;
 static NSExtensionContext* extContext;
 
+#define URL_IDENTIFIER @"public.url"
+#define IMAGE_IDENTIFIER @"public.image"
+#define TEXT_IDENTIFIER (NSString *)kUTTypePlainText
+
+NSExtensionContext* extensionContext;
+
 RCT_EXPORT_MODULE();
+
+RCT_REMAP_METHOD(data,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [self extractDataFromContext: extensionContext withCallback:^(NSDictionary* dict, NSException* err) {
+        NSLog(@"react-native-share-extension dict: %@", dict);
+        if(err) {
+            reject(@"error", err.description, nil);
+        } else {
+            resolve(dict);
+        }
+    }];
+}
+
+- (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSDictionary* dict, NSException *exception)) callback {
+    @try {
+        NSExtensionItem *item = [context.inputItems firstObject];
+        NSArray *attachments = item.attachments;
+
+        __block NSItemProvider *urlProvider = nil;
+        __block NSItemProvider *imageProvider = nil;
+        __block NSItemProvider *textProvider = nil;
+
+        [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
+            if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
+                urlProvider = provider;
+            } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
+                textProvider = provider;
+            }
+        }];
+
+        __block NSMutableDictionary* dict = [[NSMutableDictionary alloc] init];
+        __block BOOL ok = false;
+
+        if(urlProvider) {
+            [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                NSURL *url = (NSURL *)item;
+
+                [dict setObject:[url absoluteString] forKey:@"url"];
+            }];
+            ok = true;
+        }
+        if (imageProvider) {
+            [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                NSURL *url = (NSURL *)item;
+
+                [dict setObject:[url absoluteString] forKey:[[[url absoluteString] pathExtension] lowercaseString]];
+            }];
+            ok = true;
+        }
+        if (textProvider) {
+            [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                NSString *text = (NSString *)item;
+
+                [dict setObject:text forKey:@"text"];
+            }];
+            ok = true;
+        }
+
+        if (!callback) return;
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.4 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            if (ok) {
+                if (callback) {
+                    callback(dict, nil);
+                }
+            } else {
+                if (callback) {
+                    callback(nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
+                }
+            }
+        });
+    }
+    @catch (NSException *exception) {
+        if(callback) {
+            callback(nil, exception);
+        }
+    }
+}
 
 RCT_EXPORT_METHOD(getFilePath:(RCTResponseSenderBlock)callback)
 {
@@ -23,7 +108,7 @@ RCT_EXPORT_METHOD(getFilePath:(RCTResponseSenderBlock)callback)
             callback(@[url.absoluteString]);
         }];
     }
-    
+
     if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeFileURL]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeFileURL options:nil completionHandler:^(NSURL *url, NSError *error) {
             callback(@[url.absoluteString]);
@@ -34,13 +119,13 @@ RCT_EXPORT_METHOD(getFilePath:(RCTResponseSenderBlock)callback)
             callback(@[url.absoluteString]);
         }];
     }
-    
+
     if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeBMP]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeBMP options:nil completionHandler:^(NSURL *url, NSError *error) {
             callback(@[url.absoluteString]);
         }];
     }
-    
+
     if ([ShareFileIntentModule_itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeGIF]) {
         [ShareFileIntentModule_itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeGIF options:nil completionHandler:^(NSURL *url, NSError *error) {
             callback(@[url.absoluteString]);
@@ -91,7 +176,7 @@ RCT_EXPORT_METHOD(getFilePath:(RCTResponseSenderBlock)callback)
             callback(@[url.absoluteString]);
         }];
     }
-    
+
 }
 
 RCT_EXPORT_METHOD(openURL:(NSString *)url) {
@@ -103,7 +188,7 @@ RCT_EXPORT_METHOD(openURL:(NSString *)url) {
 
 RCT_EXPORT_METHOD(close)
 {
-    
+
     [ extContext completeRequestReturningItems: @[] completionHandler: nil ];
 }
 
@@ -121,4 +206,3 @@ RCT_EXPORT_METHOD(close)
     extContext = context;
 }
 @end
-
